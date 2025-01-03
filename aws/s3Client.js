68d637926bafc150ac2a6e58e647654c00f67bde
shivek-flipitnews <shivek@flipitnews.com>
2025-01-03 16:42:15 +0530

    s3 advanced functions

--- aws/s3Client.js
+++ aws/s3Client.js
@@ -1,5 +1,6 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import crypto from "crypto";
+import axios from "axios";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -17,28 +18,52 @@ const generateRandomString = (length = 32) => {
   return crypto.randomBytes(length).toString("hex").slice(0, length);
 };
 
-const s3Upload = async (file) => {
+// Main function that handles s3 upload
+const s3Upload = async (fileName, body, contentType) => {
+  const uploadParams = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: fileName,
+    Body: body,
+    ContentType: contentType,
+    CacheControl: "public, max-age=31536000",
+  };
+
+  const command = new PutObjectCommand(uploadParams);
+  await s3Client.send(command);
+};
+
+// Fetch image data from URL, then upload to s3
+const s3ImageUpload = async (imageUrl) => {
+  try {
+    // Fetch the image data from the URL
+    const response = await axios.get(imageUrl, { responseType: "arraybuffer" });
+
+    const contentType = response.headers["content-type"];
+    const randomString = generateRandomString(32);
+    const fileExtension = imageUrl.split(".").pop(); // Extract file extension
+    const fileName = `image/${randomString}.${fileExtension}`;
+
+    await s3Upload(fileName, response.data, contentType);
+
+    return fileName;
+  } catch (error) {
+    console.error("Error uploading image to S3:", error);
+  }
+};
+
+// Upload file to s3
+const s3FileUpload = async (file) => {
   try {
     const randomString = generateRandomString(32);
     const fileExtension = file.originalname.split(".").pop(); // Extract file extension
-    const fileName = `banner-image-new/${randomString}.${fileExtension}`;
+    const fileName = `image/${randomString}.${fileExtension}`;
 
-    const uploadParams = {
-      Bucket: process.env.S3_BUCKET_NAME,
-      Key: fileName,
-      Body: file.buffer,
-      ContentType: file.mimetype,
-      CacheControl: "public, max-age=31536000",
-    };
+    await s3Upload(fileName, file.buffer, file.mimetype);
 
-    const command = new PutObjectCommand(uploadParams);
-    await s3Client.send(command);
-
-    const objectURL = `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileName}`;
-    return objectURL;
+    return fileName;
   } catch (error) {
     console.error("Error uploading file to S3:", error);
   }
 };
 
-export default s3Upload;
+export { s3ImageUpload, s3FileUpload };
